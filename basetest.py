f221975680b388eb91e41fc8bdcefffd7ceab138
rodcoelho <rodcpython@gmail.com>
2018-04-03 13:11:24 -0400

    basics - non tested

--- basetest.py
+++ basetest.py
@@ -1,11 +1,38 @@
 #!/usr/bin/env python3
 
-import pymongo
+from pymongo import MongoClient
 
-try:
-    conn = pymongo.MongoClient()
-    # db = conn.userstable
-    print('sucess')
-except:
-    print("fail")
-conn.database_names()
+#### create connectionm ####
+# if working with a remote server, provide the IP Address as opposed to 'localhost'
+conn = MongoClient('localhost', 27017)
+
+
+#### create a database ####
+# option 1
+db = conn.testdb
+
+# option 2
+# db = client['testdb']
+
+
+#### access a column in the database ####
+# option 1
+column = db.column_name
+
+# option 2
+# column = db['column_name']
+
+
+#### query data in a column ####
+testdb.column_name.find()
+
+
+#### add 'documents' to a table ####
+payload = {'info1': 'data1', 'info2': 'data2', 'info3': 'data3'}
+column.insert(payload)
+
+
+#### close connection ####
+conn.close()
+
+
